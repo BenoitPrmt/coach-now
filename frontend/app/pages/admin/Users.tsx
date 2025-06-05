bd62305c889fc189ef9fd6810e28e0cd1aa3f440
Benoît <benoitparmentier45@gmail.com>
2025-06-05 14:03:34 +0200

    feat(middleware): added validation on role on specific routes

---
+++ frontend/app/pages/admin/Users.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CoachCard from "~/components/CoachCard";
+
+const UsersPage = () => {
+    return (
+        <div className="flex sm:pt-20 items-center justify-center">
+            <p>Administration des utilisateurs</p>
+        </div>
+    );
+};
+
+export default UsersPage;
