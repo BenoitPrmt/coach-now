12cec24b7e3539833bba325eed600256a99e9a38
Benoît <benoitparmentier45@gmail.com>
2025-06-10 21:04:42 +0200

    feat(user): handling partial update of user

--- api/src/main/java/com/coachnow/api/web/request/user/UserUpdate.java
+++ api/src/main/java/com/coachnow/api/web/request/user/UserUpdate.java
@@ -1,4 +1,51 @@
 package com.coachnow.api.web.request.user;
 
+import com.coachnow.api.types.Roles;
+
 public class UserUpdate {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Roles role;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Roles getRole() {
+        return role;
+    }
+
+    public void setRole(Roles role) {
+        this.role = role;
+    }
 }
